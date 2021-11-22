db19274fe2f04804edb5e944a4ab3343dc148c3d
Rivers <oscarrm96@hotmail.com>
2021-11-21 20:13:21 -0600

    Update symbol_table.py
    symbol_table is a file that is currently being worked on- added class which will be later changed

--- symbol_table.py
+++ symbol_table.py
@@ -1,1 +1,10 @@
+from compiler.outlier import *
 
+class SymbolTable:
+    __func = 'functions'
+    __sym = 'symbols'
+    __local = 'local'
+
+    __table = {
+        __func: {},__sym: {},__local: []
+    }
