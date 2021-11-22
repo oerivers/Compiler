4c4736607f9a13ce72bcc705582558e465d740d2
Rivers <oscarrm96@hotmail.com>
2021-11-21 23:42:11 -0600

    Update parser.py
    statement/identifier /exit/primitive

--- parser.py
+++ parser.py
@@ -21,3 +21,35 @@ def p_statement_list(p):
     else:
         p[1].children.append(p[2])
         p[0] = p[1]
+        
+def p_statement(p):
+    '''
+    statement : identifier
+              | expression
+              | if_statement
+    '''
+    p[0] = p[1]
+
+def p_identifier(p):
+    '''
+    identifier : IDENTIFIER
+    '''
+    p[0] = ast.Identifier(p[1])
+
+def p_exit_stmt(p):
+    '''
+    statement : EXIT STMT_END
+    '''
+    p[0] = ast.ExitStatement()
+    
+def p_primitive(p):
+    '''
+    primitive : NUM_INT
+              | NUM_FLOAT
+              | STRING
+              | boolean
+    '''
+    if isinstance(p[1], ast.BaseExpression):
+        p[0] = p[1]
+    else:
+        p[0] = ast.Primitive(p[1])
