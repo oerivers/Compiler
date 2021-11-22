bdd31439b9f44dab3ae0b127a2f8c94adc2a2b71
Rivers <oscarrm96@hotmail.com>
2021-11-22 11:44:11 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,6 +11,7 @@ precedence = (
     ('right', 'UPLUS'),
 )
 
+
 def p_statement_list(p):
     '''
     statement_list : statement
@@ -21,7 +22,8 @@ def p_statement_list(p):
     else:
         p[1].children.append(p[2])
         p[0] = p[1]
-        
+
+
 def p_statement(p):
     '''
     statement : identifier
@@ -30,18 +32,21 @@ def p_statement(p):
     '''
     p[0] = p[1]
 
+
 def p_identifier(p):
     '''
     identifier : IDENTIFIER
     '''
     p[0] = ast.Identifier(p[1])
 
+
 def p_exit_stmt(p):
     '''
     statement : EXIT STMT_END
     '''
     p[0] = ast.ExitStatement()
-    
+
+
 def p_primitive(p):
     '''
     primitive : NUM_INT
@@ -53,7 +58,7 @@ def p_primitive(p):
         p[0] = p[1]
     else:
         p[0] = ast.Primitive(p[1])
- 
+
 def p_binary_op(p):
     '''
     expression : expression PLUS expression %prec PLUS
@@ -69,7 +74,8 @@ def p_binary_op(p):
             | expression RSHIFT expression
     '''
     p[0] = ast.BinaryOperation(p[1], p[3], p[2])
-    
+
+
 def p_boolean_operators(p):
     '''
     boolean : expression EQ expression
@@ -83,6 +89,8 @@ def p_boolean_operators(p):
     '''
     p[0] = ast.BinaryOperation(p[1], p[3], p[2])
 
+
+
 def p_unary_operation(p):
     '''
     expression : MINUS expression %prec UMINUS
@@ -92,11 +100,15 @@ def p_unary_operation(p):
     '''
     p[0] = ast.UnaryOperation(p[1], p[2])
 
+
+
 def p_paren(p):
     '''
     expression : LPAREN expression RPAREN
     '''
     p[0] = p[2] if isinstance(p[2], ast.BaseExpression) else ast.Primitive(p[2])
+
+
 
 def p_boolean(p):
     '''
@@ -104,3 +116,34 @@ def p_boolean(p):
             | FALSE
     '''
     p[0] = ast.Primitive(p[1])
+
+
+def p_assignable(p):
+    '''
+    assignable : primitive
+               | expression
+    '''
+    p[0] = p[1]
+
+
+
+def p_comma_separated_expr(p):
+    '''
+    arguments : arguments COMMA expression
+              | expression
+              |
+    '''
+    if len(p) == 2:
+        p[0] = ast.InstructionList([p[1]])
+    elif len(p) == 1:
+        p[0] = ast.InstructionList()
+    else:
+        p[1].children.append(p[3])
+        p[0] = p[1]
+
+
+def p_array_access_assign(p):
+    '''
+    statement : identifier LSQBRACK expression RSQBRACK EQUALS expression STMT_END
+    '''
+    p[0] = ast.ArrayAssign(p[1], p[3], p[6])
