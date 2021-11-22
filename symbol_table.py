13647c7fbd4bc6d20e85d559d912c00bdbbae494
Rivers <oscarrm96@hotmail.com>
2021-11-21 21:12:16 -0600

    Update symbol_table.py

--- symbol_table.py
+++ symbol_table.py
@@ -8,3 +8,17 @@ class SymbolTable:
     __table = {
         __func: {},__sym: {},__local: []
     }
+
+    def __is_local(self):
+        return len(self.__table[self.__local]) > 0
+
+    def get_sym(self, sym):
+        if self.__is_local():
+            for tab in reversed(self.__table[self.__local]):
+                if sym in tab:
+                    return tab[sym]
+
+        if sym in self.__table[self.__sym]:
+            return self.__table[self.__sym][sym]
+
+        raise SymbolNotFound("Undefined variable '%s'" % sym)
