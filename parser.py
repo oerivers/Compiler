dc4019c1299c3495a9caba6fee65e19050a8ff77
Rivers <oscarrm96@hotmail.com>
2021-11-22 18:51:25 -0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -173,9 +173,41 @@ def p_ifstatement_else(p):
     '''
     p[0] = ast.If(p[2], p[4], p[8])
 
-    
+
 def p_ifstatement_else_if(p):
     '''
     if_statement : IF expression LBRACK statement_list RBRACK ELSE if_statement
     '''
     p[0] = ast.If(p[2], p[4], p[7])
+
+
+
+def p_in_expression(p):
+    '''
+    expression : expression IN expression
+               | expression NOT IN expression
+    '''
+    if len(p) == 4:
+        p[0] = ast.InExpression(p[1], p[3])
+    else:
+        p[0] = ast.InExpression(p[1], p[4], True)
+
+
+
+def p_print_statement(p):
+    '''
+    statement : PRINT arguments STMT_END
+    '''
+    p[0] = ast.PrintStatement(p[2])
+
+
+
+def p_increment_decrement_identifiers(p):
+    '''
+    expression : identifier DOUBLE_PLUS
+               | identifier DOUBLE_MINUS
+    '''
+    if p[2] == '++':
+        p[0] = ast.BinaryOperation(p[1], ast.Primitive(1), '+')
+    else:
+        p[0] = ast.BinaryOperation(p[1], ast.Primitive(1), '-')
