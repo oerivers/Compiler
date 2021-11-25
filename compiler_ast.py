97fd4bbc5ea7f491f7f10e45661531b973d7a2c3
Rivers <oscarrm96@hotmail.com>
2021-11-25 00:37:31 -0600

    Update compiler_ast.py

--- compiler_ast.py
+++ compiler_ast.py
@@ -88,7 +88,7 @@ class Assignment(BaseExpression):
     def __repr__(self):
         return '<Assignment sym={0}; val={1}>'.format(self.identifier, self.val)
 
-  
+    
     def eval(self):
         if self.identifier.is_function:
             self.identifier.assign(self.val)
@@ -116,3 +116,28 @@ class BinaryOperation(BaseExpression):
         'or': lambda a, b: a.eval() or b.eval(),
 
     }
+    
+
+    def __repr__(self):
+        return '<BinaryOperation left ={0} right={1} operation="{2}">'.format(self.left, self.right, self.op)
+
+    def __init__(self, left, right, op):
+        self.left = left
+        self.right = right
+        self.op = op
+
+    def eval(self):
+        left = None
+        right = None
+
+        try:
+            op = self.__operations[self.op]
+            if isinstance(op, LambdaType):
+                return op(self.left, self.right)
+
+            left = self.left.eval()
+            right = self.right.eval()
+            return op(left, right)
+        except TypeError:
+            fmt = (left.__class__.__name__, left, self.op, right.__class__.__name__, right)
+            raise InterpreterRuntimeError("Unable to apply operation (%s: %s) %s (%s: %s)" % fmt)
