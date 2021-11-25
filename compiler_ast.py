506269fcd78e69dcf8d3713fabafedcfe2942e35
Rivers <oscarrm96@hotmail.com>
2021-11-24 19:34:41 -0600

    Update compiler_ast.py

--- compiler_ast.py
+++ compiler_ast.py
@@ -6,3 +6,12 @@ class InstructionList:
         if children is None:
             children = []
         self.children = children
+
+    def __len__(self):
+        return len(self.children)
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def __repr__(self):
+        return '<InstructionList {0}>'.format(self.children)
