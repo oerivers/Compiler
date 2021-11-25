93a0ec8b3d6bacee239c9259db9c93ce1a0bb674
Rivers <oscarrm96@hotmail.com>
2021-11-24 19:22:49 -0600

    Update compiler_ast.py

--- compiler_ast.py
+++ compiler_ast.py
@@ -1,2 +1,8 @@
 import operator
 from types import LambdaType
+
+class InstructionList:
+    def __init__(self, children=None):
+        if children is None:
+            children = []
+        self.children = children
